aecba333a536a25a35c41af117409e0aa1d855ec
Ava Thorn <avthorn@cisco.com>
2018-03-28 17:25:41 -0400

    commands supported

--- queuebot/__init__.py
+++ queuebot/__init__.py
@@ -4,6 +4,10 @@ from config import QUEUE_BOT
 class QueueBot():
     def __init__(self):
         self.api = CiscoSparkAPI(QUEUE_BOT)
+        self.supported_commands = [
+            'add me',
+            'remove me'
+        ]
 
     def create_message(self, message, roomId):
         self.api.messages.create(text=message, roomId=roomId)
@@ -13,9 +17,16 @@ class QueueBot():
             if self.api.people.me().id in data['mentionedPeople']:
                 message_id = data['id']
                 message_text = self.api.messages.get(message_id).text
-                import pdb; pdb.set_trace()
-                # message_text = message_text.replace()
-                self.create_message(message_text, data['roomId'])
+                message_text = message_text.replace(self.api.people.me().displayName, "", 1).strip()
+
+                if message_text.lower() not in self.supported_commands:
+                    self.create_message(
+                        f"Unrecognized Command: '{message_text.lower()}'\n\n"
+                        f"Please use one of:\n" + str('\n- '.join(self.supported_commands)),
+                        data['roomId']
+                    )
+                else:
+                    self.create_message(message_text, data['roomId'])
             else:
                 return ''
         else:
